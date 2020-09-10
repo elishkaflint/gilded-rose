78d617ba949793f26ddcc86ecabdcc7c9165382b
Elishka Flint <eflint@deloitte.co.uk>
2020-09-10 18:51:58 +0100

    Test to cover "Aged Brie" actually increases in Quality the older it gets

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -21,4 +21,20 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(8, app.items[0].quality);
     }
+
+    @Test
+    public void qualityCannotBeNegative() {
+        Item[] items = new Item[] { new Item("foo", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void agedBrie_increasesInQuality() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(6, app.items[0].quality);
+    }
 }
