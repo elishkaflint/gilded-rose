e7b59d0fd8c9d0fae896cebc070c620c2d5a6649
Elishka Flint <eflint@deloitte.co.uk>
2020-09-10 18:48:23 +0100

    Test to cover Once the sell by date has passed, Quality degrades twice as fast

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void beforeSellByDate_qualityDegradesByOne() {
+        Item[] items = new Item[] { new Item("foo", 1, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, app.items[0].quality);
     }
 
+    @Test
+    public void afterSellByDate_qualityDegradesTwiceAsFast() {
+        Item[] items = new Item[] { new Item("foo", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
 }
