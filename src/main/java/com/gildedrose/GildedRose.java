1105c823d311efe601ab7ee73c414b6e017934ef
Elishka Flint <eflint@deloitte.co.uk>
2020-09-10 22:35:57 +0100

    Satisfying refactor of the gilded class object, items now nicely encapsulated

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,78 +8,9 @@ class GildedRose {
     }
 
     public void updateQuality() {
-
         for (int i = 0; i < items.length; i++) {
-
-            if(isSulfuras(i)) {
-                continue;
-            }
-
-            items[i].sellIn = items[i].sellIn - 1;
-
-            if(isAgedBrie(i)) {
-                if(items[i].quality < 50) {
-                    if(isPastSellByDate(i)) {
-                        if(items[i].quality < 50) {
-                            increaseQuality(i);
-                        }
-                    }
-                    increaseQuality(i);
-                }
-
-            } else if(isBackstagePass(i)) {
-                if(isPastSellByDate(i)) {
-                    reduceQualityToZero(i);
-                } else {
-                    if(items[i].quality < 50) {
-                        increaseQuality(i);
-                    }
-                    if (items[i].sellIn < 10) {
-                        increaseQuality(i);
-                    }
-                    if (items[i].sellIn < 5) {
-                        increaseQuality(i);
-                    }
-                }
-
-            } else {
-                if(isPastSellByDate(i)) {
-                    reduceQuality(i);
-                }
-                reduceQuality(i);
-            }
-
+            items[i].updateQuality();
         }
-    }
-
-    private boolean isAgedBrie(int i) {
-        return items[i].name.equals("Aged Brie");
-    }
-
-    private boolean isBackstagePass(int i) {
-        return items[i].name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isSulfuras(int i) {
-        return items[i].name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isPastSellByDate(int i) {
-        return items[i].sellIn < 0;
-    }
-
-    private void reduceQuality(int i) {
-        if (items[i].quality > 0) {
-            items[i].quality = items[i].quality - 1;
-        }
-    }
-
-    private void increaseQuality(int i) {
-        items[i].quality = items[i].quality + 1;
-    }
-
-    private void reduceQualityToZero(int i) {
-        items[i].quality = items[i].quality - items[i].quality;
     }
 
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,18 +9,25 @@ class GildedRoseTest {
 
     @Test
     public void testMultipleItems() {
-        Item agedBrie = new com.gildedrose.AgedBrie("Aged Brie", 5, 5);
-        Item backstageTicket = new BackstageTicket("Backstage passes to a TAFKAL80ETC concert", 11, 5);
-        Item normalItem = new com.gildedrose.NormalItem("foo", 1, 10);
-        Item[] items = new Item[] {agedBrie, backstageTicket, normalItem};
+
+        Item agedBrie = new Item("Aged Brie", 5, 5);
+        Item backstageTicket = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 5);
+        Item normalItem = new Item("foo", 1, 10);
+        Item sulphuras = new Item("Sulfuras, Hand of Ragnaros", 5, 5);
+
+        Item[] items = new Item[] {agedBrie, backstageTicket, normalItem, sulphuras};
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+
         assertEquals(6, app.items[0].quality);
         assertEquals(4, app.items[0].sellIn);
         assertEquals(6, app.items[1].quality);
         assertEquals(10, app.items[1].sellIn);
         assertEquals(9, app.items[2].quality);
         assertEquals(0, app.items[2].sellIn);
+        assertEquals(5, app.items[3].quality);
+        assertEquals(5, app.items[3].sellIn);
 
     }
 
