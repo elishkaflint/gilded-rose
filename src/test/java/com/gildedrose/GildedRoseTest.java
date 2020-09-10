3766bbf183ce6658496c31c39901bb1978cd1257
Elishka Flint <eflint@deloitte.co.uk>
2020-09-10 18:57:05 +0100

    Test to cover sulfuras stays the same

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -31,10 +31,27 @@ class GildedRoseTest {
     }
 
     @Test
+    public void qualityCannotBeMoreThanFifty() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
     public void agedBrie_increasesInQuality() {
         Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(6, app.items[0].quality);
+    }
+
+    @Test
+    public void sulfuras_staysTheSame() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(5, app.items[0].quality);
+        assertEquals(5, app.items[0].sellIn);
     }
 }
